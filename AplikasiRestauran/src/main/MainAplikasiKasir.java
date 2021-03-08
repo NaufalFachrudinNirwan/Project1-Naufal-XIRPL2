c66d367c59ca304ce090ac1b6673ce2915f4b00b
Naufal Fachrudin Nirwan <nafach1327@gmail.com>
2021-03-08 09:19:54 +0700

    Modul D: Inisiasi Transaksi

--- AplikasiRestauran/src/main/MainAplikasiKasir.java
+++ AplikasiRestauran/src/main/MainAplikasiKasir.java
@@ -7,11 +7,20 @@ import java.util.Scanner;
 public class MainAplikasiKasir {
 
     public DaftarMenu daftarMenu;
+    //Tambahkan
+    public static double PAJAK_PPN = 0.10;
+    public static double BIAYA_SERVICE = 0.05;
+    //End Of Tambahkan
 
     public static void  main(String[] args) {
         //inisialisasi kelas Scanner untuk mengambil
         //input dari keyboard
         Scanner input = new Scanner(System.in);
+        //Tambahkan
+        String no_transaksi, nama_pemesan, tanggal, no_meja = "";
+        String transaksi_lagi = "", pesan_lagi = "", keterangan = "", makan_ditempat;
+        int jumlah_pesanan, no_menu;
+        //End Of Tambahkan
 
         MainAplikasiKasir app = new MainAplikasiKasir();
         //tampilkan daftar menu
